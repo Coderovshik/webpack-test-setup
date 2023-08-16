d668e9840d60e99ef5ba66293c04646c3f128833
Coderovshik <peter.04007@gmail.com>
2023-08-16 21:49:47 +0300

    use image

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import _ from 'lodash';
 // styles
 import './styles/index.css';
 
+// images
+import amogus from './assets/amogus.jpg';
+
 function component() {
     const div = document.createElement('div');
+    const img = document.createElement('img');
 
     div.innerHTML = _.join(['Biba', 'Boba'], ' | ');
+    img.src = amogus;
+
+    div.appendChild(img);
     
     return div;
 }
